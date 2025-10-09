92160e0ffb36c1f10a74a261b22eed319a99f7a0
michaelouma <moketchus12@gmail.com>
2025-10-09 11:10:29 +0300

    Getting started JS

---
+++ JavaScript/script.js
@@ -0,0 +1,23 @@
+const Birthyear = 2003
+console.log("I was born in " + Birthyear)
+
+// Comparison operator
+console.log(5 =="5")
+console.log(5 === "5")
+
+// Functions
+
+function greet(){
+    console.log("Hello am Michael")
+}
+greet()
+
+// 2. use of a function expression
+ const  Hello = function() {
+    console.log("This is a function expression")
+ }
+Hello()
+
+// 3. use of a modern arrow function
+const Goodbye = () => console.log("Goodbye from this side,,this is a modern arrow function")
+Goodbye()
